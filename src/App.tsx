698c4d8a1e1569563a2821294dd489465c4b8669
Aditya Rai <raiaditya000786@gmail.com>
2022-08-07 14:36:04 +0530

    Final Changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Adroit App Changing on Local</p>
+        <p>Adroit App Final Changes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
